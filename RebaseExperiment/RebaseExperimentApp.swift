1472a9f4f915f3237aee3bbbf91cf2ceace6a70e
generic-keith <keith.staines.ext@bbc.co.uk>
2023-09-23 12:37:32 +0100

    Initial Commit

---
+++ RebaseExperiment/RebaseExperimentApp.swift
@@ -0,0 +1,17 @@
+//
+//  RebaseExperimentApp.swift
+//  RebaseExperiment
+//
+//  Created by Keith Staines on 23/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct RebaseExperimentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
