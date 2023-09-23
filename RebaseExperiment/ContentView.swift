9f014e6e81bfa4d6aaee221d8a1ec34a6ed34341
generic-keith <keith.staines.ext@bbc.co.uk>
2023-09-23 12:45:02 +0100

    added some changes to main and pushed

--- RebaseExperiment/ContentView.swift
+++ RebaseExperiment/ContentView.swift
@@ -7,6 +7,8 @@
 
 import SwiftUI
 
+// added this comment on main
+
 struct ContentView: View {
     var body: some View {
         VStack {
---
+++ RebaseExperiment/MainView.swift
@@ -0,0 +1,10 @@
+//
+//  MainView.swift
+//  RebaseExperiment
+//
+//  Created by Keith Staines on 23/09/2023.
+//
+
+import Foundation
+
+// added this file on main
